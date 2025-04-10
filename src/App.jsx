021f667c7d4055ac54b9f84fe0ff3d5c3cd3eeb8
owrkl <adnanroomieh@gmail.com>
2025-04-10 19:19:37 +0300

    Final dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css';
+import Clock from './components/Clock';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Welcome to the Clock App by Adnan</h1>
+        <Clock />
       </header>
     </div>
   );
